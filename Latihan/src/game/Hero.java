8d14056c2dc84ad33a640879808f5e9cf51cae1e
Neila <neila.asynur@gmail.com>
2026-04-07 16:13:52 +0700

    menyelesaikan soal 1

---
+++ Latihan/src/game/Hero.java
@@ -0,0 +1,14 @@
+package game;
+
+public class Hero {
+    String nama;
+    double health;
+    double attackPower;
+
+    //untuk soal 1
+    public Hero (String nama, double health, double attackPower){
+        this.nama = nama;
+        this.health = health;
+        this.attackPower = attackPower;
+    }
+}
