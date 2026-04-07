e8800325b6ba4fcc2ebee4eb54f65606bd881d91
Neila <neila.asynur@gmail.com>
2026-04-07 15:10:33 +0700

    upload pre test modul 6

---
+++ PreTest/src/Main.java
@@ -0,0 +1,8 @@
+import minimarket.MakananRingan;
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+        MakananRingan cemilan = new MakananRingan("nabati", 8000, "keju");
+        cemilan.tampilkanDetail();
+    }
+}
