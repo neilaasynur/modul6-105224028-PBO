cf8cf215c102c8c10befbb37ae1c6eddec571bdf
Neila <neila.asynur@gmail.com>
2026-04-07 16:19:58 +0700

    menjawab nomor 2

--- Latihan/src/game/Hero.java
+++ Latihan/src/game/Hero.java
@@ -11,4 +11,22 @@ public class Hero {
         this.health = health;
         this.attackPower = attackPower;
     }
+
+    //untuk soal 2
+    public void display(){
+        System.out.println("Nama: " + this.nama);
+        System.out.println("Health: " + this.health);
+        System.out.println("Attack Power: " + this.attackPower); 
+    }
+
+    public void berlatih(){
+        this.attackPower += 10;
+        System.out.println(nama + " sedang berlatih!");
+    }
+
+    public void terimaSerangan (double damage){
+        this.health -= damage;
+        System.out.println(nama + " menerima serangan sebesar " + damage);
+    }
+
 }
